13827230b06c5a5a0683c1ffc33940633d3ba54a
duongdtd <90745709+duongdtd@users.noreply.github.com>
2022-07-10 18:22:55 +0700

    update
    run shell.sh

--- Week1/shell.sh
+++ Week1/shell.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo "1000000">freq.txt
-sleep 5
+sleep 60
 echo "100000">freq.txt
-sleep 5
+sleep 60
 echo "10000">freq.txt
-sleep 5 
+sleep 60
 echo "1000">freq.txt
-sleep 5
+sleep 60
 echo "100">freq.txt
-sleep 5 && kill -9 `ps ax | grep ./b1 | grep -v grep| awk '{print $1}'`
+sleep 60 && kill -9 `ps ax | grep ./b1 | grep -v grep| awk '{print $1}'`
