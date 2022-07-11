217491858709620c1c5308ef1011cb8c1fa5a5b6
duongdtd <90745709+duongdtd@users.noreply.github.com>
2022-07-11 19:21:29 +0700

    Update shell.sh

--- Week1/shell.sh
+++ Week1/shell.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo "1000000">freq.txt
-sleep 60
+timeout 60s ./b1
 echo "100000">freq.txt
-sleep 60
+timeout 60s ./b1
 echo "10000">freq.txt
-sleep 60
+timeout 60s ./b1
 echo "1000">freq.txt
-sleep 60
+timeout 60s ./b1
 echo "100">freq.txt
-sleep 60 && kill -9 `ps ax | grep ./b1 | grep -v grep| awk '{print $1}'`
+timeout 60s ./b1
