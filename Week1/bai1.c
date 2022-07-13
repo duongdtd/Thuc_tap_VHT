ef9d9cd8e94f7f51dd35306966f10a7a1bcff144
duongdtd <90745709+duongdtd@users.noreply.github.com>
2022-07-13 23:38:41 +0700

    update _ new

--- Week1/bai1.c
+++ Week1/bai1.c
@@ -11,37 +11,28 @@ struct timespec tmp;
 long freq = 1;
 struct timespec time1, time2;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;   
- long get_freq()
-{
-  long data;
-  FILE *fp;
-  fp = fopen("freq.txt","r");
-  char buff[100];
-  fgets(buff,sizeof(buff),fp);
-  char *eptr;
-  data = strtol(buff,&eptr,10);
-  fclose(fp);
-  return data;
- }
  void *getTime(void *args )
 {   
-    clock_gettime(CLOCK_REALTIME,&tp);  
+    clock_gettime(CLOCK_REALTIME,&tp);
     return NULL;
+
 }
 void *getFreq(void *args)
 {
-  pthread_mutex_lock(&mutex);
-  long new_freq = get_freq();
+    pthread_mutex_lock(&mutex);
+  FILE *fp;
+  fp = fopen("freq.txt","r");
+   unsigned long new_freq = 0;
+  fscanf(fp,"%lu",&new_freq);
+  fclose(fp); 
   if(new_freq == freq)
-  {  
-    pthread_mutex_unlock(&mutex);
+  {  pthread_mutex_unlock(&mutex);
     return NULL;
   }
     else
     {
     freq = new_freq;
-    time1.tv_sec = 0;
-    time1.tv_nsec = freq;
+    time1.tv_nsec = new_freq;
     pthread_mutex_unlock(&mutex);
     return NULL;
     } 
@@ -49,32 +40,31 @@ void *getFreq(void *args)
 }
 void *save_time(void *args)
 {
-  struct timespec *tpx = (struct timespec *)args;
   //save time
-  if(tmp.tv_nsec != tpx->tv_nsec | tmp.tv_sec != tpx->tv_sec)
+  if(tmp.tv_nsec != tp.tv_nsec || tmp.tv_sec != tp.tv_sec)
   {
     FILE *file;
-    file = fopen("time_and_interval.txt","a+");
-    long diff_sec = ((long) tpx->tv_sec) - tmp.tv_sec ;
+    file = fopen("freq_100000.txt","a");
+    long diff_sec = ((long) tp.tv_sec) - tmp.tv_sec ;
     long diff_nsec;
 
-    if(tpx->tv_nsec > tmp.tv_nsec)
+    if(tp.tv_nsec > tmp.tv_nsec)
       {
-        diff_nsec = tpx->tv_nsec - tmp.tv_nsec;
+        diff_nsec = tp.tv_nsec - tmp.tv_nsec;
       }
     else 
       {
-        diff_nsec = 1000000000 + tpx->tv_nsec - tmp.tv_nsec ;
+        diff_nsec = 1000000000 + tp.tv_nsec - tmp.tv_nsec ;
         diff_sec = diff_sec - 1;
       }
-    fprintf(file,"\n%ld.%09ld\n%ld.%09ld",tpx->tv_sec,tpx->tv_nsec,diff_sec,diff_nsec);  
+     fprintf(file,"\n%ld.%09ld",diff_sec,diff_nsec);  
     fclose(file);
-    tmp.tv_sec = tpx->tv_sec;
-    tmp.tv_nsec = tpx->tv_nsec;
+    tmp.tv_sec = tp.tv_sec;
+    tmp.tv_nsec = tp.tv_nsec;
     return NULL;
   }
   else
-  {
+  {  
     return NULL;
   }
 }
@@ -91,25 +81,23 @@ int main(int argc, char const *argv[])
 
       tmp.tv_sec = 0;
       tmp.tv_nsec = 0;   
-       pthread_mutex_init(&mutex, NULL); 
-   
+    pthread_mutex_init(&mutex, NULL); 
       while(1)
         {
 
         if(nanosleep(&time1 , &time2) < 0 )   
             {
-        printf("Nano sleep system call failed \n");
         return -1;
       }
         else
           { 
         i = pthread_create(&INPUT,NULL,getFreq,NULL);
         s = pthread_create(&SAMPLE, NULL, getTime,NULL);
-        l = pthread_create(&LOGGING,NULL,save_time,&tp);      
+        l = pthread_create(&LOGGING,NULL,save_time,NULL);      
         pthread_join(INPUT,NULL);
         pthread_join(SAMPLE,NULL);
         pthread_join(LOGGING,NULL);
-        pthread_mutex_destroy(&mutex);
+           pthread_mutex_destroy(&mutex);
       }
-        } return 0;
+        } 
 }
