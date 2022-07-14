07ef018d499c6a5f34d93bce1dc4abc2356e4093
duongdtd <90745709+duongdtd@users.noreply.github.com>
2022-07-14 14:44:33 +0700

    update file.c

--- Week1/bai1.c
+++ Week1/bai1.c
@@ -3,23 +3,68 @@
 #include"pthread.h"
 #include"time.h"
 #include"string.h"
+#include"stdbool.h"
 #include"stdlib.h"
 #include "unistd.h"
 struct timespec tp;
-struct timespec tmp;
-
-long freq = 1;
-struct timespec time1, time2;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;   
+struct timespec tmp,request1;
+uint8_t check_loop = 1;
+long freq = 1000000;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;  
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;   
  void *getTime(void *args )
 {   
-    clock_gettime(CLOCK_REALTIME,&tp);
-    return NULL;
+
+  while(check_loop == 1) 
+  {      
+      clock_gettime(CLOCK_REALTIME,&tp);  
+      printf("\n%ld.%09ld",tp.tv_sec,tp.tv_nsec); 
+      request1.tv_nsec=tp.tv_nsec;
+      request1.tv_sec=tp.tv_sec; 
+      long temp;
+        if(request1.tv_nsec + freq > 1000000000)
+        {
+          temp = request1.tv_nsec;
+          request1.tv_nsec =0;
+          request1.tv_sec +=1;
+            if(clock_nanosleep(CLOCK_REALTIME,TIMER_ABSTIME, &request1,NULL) != 0)
+              {
+                  check_loop = 0;
+              }
+              else
+              {
+                request1.tv_nsec+=temp-1000000000 + freq;
+                 if(clock_nanosleep(CLOCK_REALTIME,TIMER_ABSTIME, &request1,NULL) != 0)
+                  {
+                      check_loop = 0;
+                  }
+                  else
+                  {
+                      check_loop = 1;
+                  }
+              }
+        }
+        else{
+            request1.tv_nsec +=freq;
+            if(clock_nanosleep(CLOCK_REALTIME,TIMER_ABSTIME, &request1,NULL) != 0)
+              {
+                  check_loop = 0;
+              }
+            else
+              {
+                  check_loop = 1;
+              }
+        }
+        
+  }
 
 }
 void *getFreq(void *args)
 {
-    pthread_mutex_lock(&mutex);
+  while(1)
+  {
+
+  pthread_mutex_lock(&mutex);
   FILE *fp;
   fp = fopen("freq.txt","r");
    unsigned long new_freq = 0;
@@ -27,46 +72,42 @@ void *getFreq(void *args)
   fclose(fp); 
   if(new_freq == freq)
   {  pthread_mutex_unlock(&mutex);
-    return NULL;
   }
     else
-    {
+  {
     freq = new_freq;
-    time1.tv_nsec = new_freq;
     pthread_mutex_unlock(&mutex);
-    return NULL;
-    } 
+  } 
+  }
 
 }
 void *save_time(void *args)
 {
   //save time
-  if(tmp.tv_nsec != tp.tv_nsec || tmp.tv_sec != tp.tv_sec)
+  while(1)
   {
     FILE *file;
-    file = fopen("freq_100000.txt","a");
+    file = fopen("freq_1000000.txt","a+");
     long diff_sec = ((long) tp.tv_sec) - tmp.tv_sec ;
     long diff_nsec;
+    if(tmp.tv_nsec != tp.tv_nsec || tmp.tv_sec != tp.tv_sec)
+    {
+      if(tp.tv_nsec > tmp.tv_nsec)
+        {
+          diff_nsec = tp.tv_nsec - tmp.tv_nsec;
+        }
+      else 
+        {
+          diff_nsec = 1000000000 + tp.tv_nsec - tmp.tv_nsec ;
+          diff_sec = diff_sec - 1;
+        }
+       fprintf(file,"\n%ld.%09ld",diff_sec,diff_nsec);  
+       tmp.tv_nsec =tp.tv_nsec;
+       tmp.tv_sec = tp.tv_sec;
 
-    if(tp.tv_nsec > tmp.tv_nsec)
-      {
-        diff_nsec = tp.tv_nsec - tmp.tv_nsec;
-      }
-    else 
-      {
-        diff_nsec = 1000000000 + tp.tv_nsec - tmp.tv_nsec ;
-        diff_sec = diff_sec - 1;
-      }
-     fprintf(file,"\n%ld.%09ld",diff_sec,diff_nsec);  
+    } 
     fclose(file);
-    tmp.tv_sec = tp.tv_sec;
-    tmp.tv_nsec = tp.tv_nsec;
-    return NULL;
-  }
-  else
-  {  
-    return NULL;
-  }
+  } 
 }
 
 int main(int argc, char const *argv[])
@@ -76,28 +117,15 @@ int main(int argc, char const *argv[])
         pthread_t INPUT;
         pthread_t LOGGING;
         int s,i,l;
-      time1.tv_sec = 0;
-      time1.tv_nsec = freq;
-
       tmp.tv_sec = 0;
       tmp.tv_nsec = 0;   
     pthread_mutex_init(&mutex, NULL); 
-      while(1)
-        {
-
-        if(nanosleep(&time1 , &time2) < 0 )   
-            {
-        return -1;
-      }
-        else
-          { 
-        i = pthread_create(&INPUT,NULL,getFreq,NULL);
+       i = pthread_create(&INPUT,NULL,getFreq,NULL);
         s = pthread_create(&SAMPLE, NULL, getTime,NULL);
-        l = pthread_create(&LOGGING,NULL,save_time,NULL);      
-        pthread_join(INPUT,NULL);
+       l = pthread_create(&LOGGING,NULL,save_time,NULL);      
+       pthread_join(INPUT,NULL);
         pthread_join(SAMPLE,NULL);
-        pthread_join(LOGGING,NULL);
+       pthread_join(LOGGING,NULL);
            pthread_mutex_destroy(&mutex);
-      }
-        } 
+    
 }
